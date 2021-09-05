7e44d0a3952ad136600ecde86213da2b25d4ac0c
kimdahin <87987561+kimdahin@users.noreply.github.com>
2021-09-06 01:01:18 +0900

    ModelessTypeDialogPracAndTimer
    모달리스형 다이얼로그를 생성하고, 
    해제(그 다이얼로그 창 닫기 버튼이 눌렸을 때),
     그리고 메뉴에서 다이얼로그를 해제,
    다이얼로그 생성시 처리, 
    타이머를 이용해서 프로그램 실행 후 몇 초 뒤에 모달리스형 창을 실행했는지,
    등

---
+++ ModelessDialogPracAndTimer/WindowsProject1/WindowsProject1.cpp
@@ -0,0 +1,243 @@
+﻿// WindowsProject1.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "framework.h"
+#include "WindowsProject1.h"
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+TCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+TCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+//이것은 모달리스형 다이얼로그의 핸들입니다. 
+//그 다이얼로그의 윈도우 창을 핸들로 가지고 있다고 생각하면 될 것 같습니다.
+//이렇게 전역변수로 선언하면 상위의 윈도우 창에서 이 윈도우 창을 제어할 수 있습니다.(이 창을 닫을 수 있다, 등)
+HWND g_hModelessWnd;
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadString(hInstance, IDC_WINDOWSPROJECT1, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 애플리케이션 초기화를 수행합니다:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT1));
+
+    MSG msg;
+
+    // 기본 메시지 루프입니다:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEX wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT1));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCE(IDR_MENU1);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassEx(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   HWND hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+int time = 0;
+char timeStr[100];
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        SetTimer(hWnd, 1, 1000, 0);
+        break;
+    case WM_TIMER:
+        switch (wParam) {
+        case 1:
+            time++;
+            break;
+        }
+        InvalidateRect(hWnd, NULL, TRUE);
+        break;
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case ID_32773:
+                //모달리스형 다이얼로그를 생성할 때 
+                //전역변수로 선언해 둔 이 다이얼로그의 핸들에
+                //진짜로 생성을 해주고 그 값을 보내줍니다. 그럼 상위의 윈도우 창이 이 창을 제어할 수 있습니다.
+                g_hModelessWnd = CreateDialog(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, DlgProc);
+                break;
+            case ID_32777:
+                //이와 같이 부모 윈도우 창에서도 모달리스형 다이얼로그를 해지시킬 수 있습니다.
+                DestroyWindow(g_hModelessWnd);
+                break;
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+            sprintf(timeStr, "지난 시간: %d", time);
+            TextOut(hdc, 10, 10, timeStr, strlen(timeStr));
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        KillTimer(hWnd, 1);
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+char string[100];
+// 모달리스형 다이얼로그의 메시지 처리기
+INT_PTR CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        sprintf(string, "프로그램을 실행하고 %d초 뒤에 이 창을 실행했습니다.", time);
+        return (INT_PTR)TRUE;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hDlg, &ps);
+        
+        //출력
+        TextOut(hdc, 10, 10, string, strlen(string));
+
+        EndPaint(hDlg, &ps);
+    }
+    return (INT_PTR)TRUE;
+    case WM_CLOSE:
+        //닫기 버튼을 누르면 WM_CLOSE 메시지가 발생되며
+        //모달리스형 다이얼로그를 해지함. hDlg는 이 모달리스형의 윈도우 핸들
+        DestroyWindow(hDlg);
+        return (INT_PTR)TRUE;
+    }
+
+    return (INT_PTR)FALSE;
+}
